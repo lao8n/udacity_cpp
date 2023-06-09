6ba9d4f2a86c991ebdcf516958c54e98abd22fae
Nicholas Drake <hellolao8n@gmail.com>
2023-06-09 19:13:39 +0100

    add working cmake example for windows

---
+++ cpp_foundations/multifile_programs/cmake/build/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lao8n/OneDrive/Documents/udacity_cpp/cpp_foundations/multifile_programs/cmake/src/increment_and_sum.cpp" "CMakeFiles/example.dir/src/increment_and_sum.cpp.obj" "gcc" "CMakeFiles/example.dir/src/increment_and_sum.cpp.obj.d"
+  "C:/Users/lao8n/OneDrive/Documents/udacity_cpp/cpp_foundations/multifile_programs/cmake/src/main.cpp" "CMakeFiles/example.dir/src/main.cpp.obj" "gcc" "CMakeFiles/example.dir/src/main.cpp.obj.d"
+  "C:/Users/lao8n/OneDrive/Documents/udacity_cpp/cpp_foundations/multifile_programs/cmake/src/vect_add_one.cpp" "CMakeFiles/example.dir/src/vect_add_one.cpp.obj" "gcc" "CMakeFiles/example.dir/src/vect_add_one.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp_foundations/multifile_programs/cmake/build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/src/increment_and_sum.cpp.obj"
+  "CMakeFiles/example.dir/src/increment_and_sum.cpp.obj.d"
+  "CMakeFiles/example.dir/src/main.cpp.obj"
+  "CMakeFiles/example.dir/src/main.cpp.obj.d"
+  "CMakeFiles/example.dir/src/vect_add_one.cpp.obj"
+  "CMakeFiles/example.dir/src/vect_add_one.cpp.obj.d"
+  "example.exe"
+  "example.exe.manifest"
+  "example.pdb"
+  "libexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
